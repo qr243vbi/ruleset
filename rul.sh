aec6aca703f0d806a13df38a01658bbb59d47ed7
u <u>
2025-10-31 12:14:20 +0300

    update

--- rul.sh
+++ rul.sh
@@ -5,9 +5,18 @@ mkdir rule-set ||:
 cd rule-set
 ../a.out | python3 ../py.py
 
+cd ..
+mkdir routes
+cd routes
+curl -s https://api.github.com/repos/throneproj/routeprofiles/releases/latest \
+  | grep "browser_download_url" \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -LO
 
 cd ..
 protoc lib.proto --cpp_out=.
 g++ test.cpp -o test `pkg-config --libs --cflags protobuf`
 ./test
+
 cd rule-set
+
