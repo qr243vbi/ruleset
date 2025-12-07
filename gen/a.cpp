633528b7a8ba031cc79165cbe2c1f1c0a7678fe8
qr243vbi <qr243vbi@atomicmail.io>
2025-12-07 11:48:44 +0300

    Update a.cpp

--- gen/a.cpp
+++ gen/a.cpp
@@ -5,6 +5,11 @@
 
 int main(){
   for (auto pair : ruleSetMap){
+     std::string originalString = pair.second;
+     if (originalString.length() >= 4 && originalString.substr(originalString.length() - 4) == ".srs") {
+       originalString.replace(originalString.length() - 4, 4, ".json");
+     }
+     pair.second = originalString;
      std::cout << pair.first << std::endl <<pair.second << std::endl;
   }
   std::cout << "" << std::endl << "" << std::endl;
