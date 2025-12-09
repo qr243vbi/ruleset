bed4e7dc963a7e05122470e9585f0f8f1e5d8042
qr243vbi <qr243vbi@atomicmail.io>
2025-12-09 09:05:29 +0300

    Update a.cpp

--- gen/a.cpp
+++ gen/a.cpp
@@ -12,5 +12,7 @@ int main(){
 //     pair.second = originalString;
      std::cout << pair.first << std::endl <<pair.second << std::endl;
   }
+  std::cout << "nekobox-adblocksingbox" << std::endl << 
+    "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblocksingbox.srs" << std::endl;
   std::cout << "" << std::endl << "" << std::endl;
 }
